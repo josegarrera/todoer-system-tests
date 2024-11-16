f16bc6d35afea006c2725dd4b595ec761d6156af
Jose Garrera <josegarrera@gmail.com>
2024-11-16 15:35:58 -0300

    testing: add smoke tests

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.*ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+};
+
+export default config;
--- tests/smoke/smoke.test.ts
+++ tests/smoke/smoke.test.ts
@@ -1,3 +1,50 @@
-test('Smoke Test Placeholder', () => {
-  expect(1).toBe(1);
+import axios from 'axios';
+import { Builder, WebDriver } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome';
+
+const BASE_URL = 'http://35.209.176.57';
+describe('UI Smoke Test', () => {
+  let driver: WebDriver;
+
+  beforeAll(async () => {
+    const options = new chrome.Options();
+    options.addArguments('--headless');
+    options.addArguments('--disable-gpu');
+    options.addArguments('--no-sandbox');
+    options.addArguments('--disable-dev-shm-usage');
+
+    driver = await new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(options)
+      .build();
+  });
+
+  afterAll(async () => {
+    if (driver) {
+      await driver.quit();
+    }
+  });
+
+  test('should load the page and check the title', async () => {
+    await driver.get(BASE_URL);
+    const title = await driver.getTitle();
+    expect(title).toContain('TODO List');
+  });
+});
+
+async function checkHealth(port: number) {
+  const response = await axios.get(`${BASE_URL}:${port}/health`);
+  return response.status === 200;
+}
+
+test('Smoke Test: Task Manager health check', async () => {
+  const TASK_MANAGER_PORT = 3001;
+  const isHealthy = await checkHealth(TASK_MANAGER_PORT);
+  expect(isHealthy).toBe(true);
+});
+
+test('Smoke Test: Random Title Generator health check', async () => {
+  const RANDOM_TITLE_GENERATOR_PORT = 3002;
+  const isHealthy = await checkHealth(RANDOM_TITLE_GENERATOR_PORT);
+  expect(isHealthy).toBe(true);
 });
